eb286b2da298f3f4b808c445392595fadc831857
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-21 17:21:34 +0700

    add void swap

--- QuickSort051/Program.cs
+++ QuickSort051/Program.cs
@@ -44,11 +44,15 @@ namespace QuickSort051
         //swaps the element at index x with the element at index y
         void swap(int x, int y)
         {
-
+            int temp;
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
         }
 
 
-        }
+
+        
     }
 }
 
