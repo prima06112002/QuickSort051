74da4c7fce4708f030dd26dfdef2678394466bd2
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-21 17:43:11 +0700

    add q_short

--- QuickSort051/Program.cs
+++ QuickSort051/Program.cs
@@ -97,7 +97,13 @@ namespace QuickSort051
             {
                 //move the pivot to its correct position in the list
                 swap(low, j);
+                mov_count++;
             }
+            //sort the list on the left of pivot using quick sort
+            q_short(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_short(j + 1, low);
 
         }
 
