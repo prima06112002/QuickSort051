0e8bb6118729d44e3f50542917e2153faabe8fa9
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-21 17:06:48 +0700

    add class void input

--- QuickSort051/Program.cs
+++ QuickSort051/Program.cs
@@ -16,6 +16,21 @@ namespace QuickSort051
         // Number of element in array
         private int n;
 
+
+        void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of element in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\narray can have maximum 20 element \n");
+            }
+        }
+
       
 
 
