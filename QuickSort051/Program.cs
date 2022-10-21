549f78bc99e305f59f41c5aa5c0190355f97e21a
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-21 16:58:26 +0700

    Add project files.

---
+++ QuickSort051/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort051
+{
+    class program
+    { 
+    //aray of integers to hold values
+    private int[] arr = new int[20];
+    private int cmp_count = 0; // number of comparasion
+    private int mov_count = 0; // number of data movement
+
+      
+
+
